9c561ae0ac50a728dbdbf8f61701919fadd74763
JuanMDSK <jzuluaga458@soyudemedellin.edu.co>
2025-09-02 11:59:15 -0500

    GameManager
    tratando de asignarle a las trampas codigo (no funcionó)

--- GGWPGame/Assets/Main/Code/Scripts/GameManager.cs
+++ GGWPGame/Assets/Main/Code/Scripts/GameManager.cs
@@ -3,6 +3,30 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private int _puntos ;
+    private int _puntos;
+    [SerializeField]
+    private int _vida = 10;
+
+    public void SumarPuntos(int cantidad)
+    {
+        _puntos += cantidad;
+
+    }
+
+
+
+    public void RestarVida(int cantidad)
+    {
+        if (_vida <= 0)
+        {
+
+            SceneManager.LoadScene(0);
+        }
+
+        else
+        {
+            _vida -= cantidad;
+        }
+    }
 
 }
