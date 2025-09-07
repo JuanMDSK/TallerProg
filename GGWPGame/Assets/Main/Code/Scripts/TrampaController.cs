a6c434ba827cbcd59b1b9fae22728d11c9469ad9
JuanMDSK <jzuluaga458@soyudemedellin.edu.co>
2025-09-07 12:58:34 -0500

    casi final
    trampas e item (falta mas trampas y mas items :b)

--- GGWPGame/Assets/Main/Code/Scripts/TrampaController.cs
+++ GGWPGame/Assets/Main/Code/Scripts/TrampaController.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class TrampaController : MonoBehaviour
 {
+    public int damage = 1; // cuánta vida quita la trampa
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Resta vida al jugador
+            GameManager.instance.TakeDamage(damage);
+        }
+    }
 }
